506868690aacd300cf270f882b90876dc9458996
Sanjana Panda <sanjana.pda75@gmail.com>
2026-05-10 12:06:40 +0000

    done

---
+++ word-letter-counter/src/App.js
@@ -0,0 +1,21 @@
+// App.js
+import React from "react";
+import WordLetterCounter from "./WordLetterCounter";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1 id="top">
+            Sanjana panda
+            </h1>
+            <h1>
+            Words and Letters
+            Counter
+            </h1>
+            <WordLetterCounter />
+        </div>
+    );
+}
+
+export default App;
